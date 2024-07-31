84d1f5241e1a9b7fe53bd09306f1b806b9a54436
Biruk Lemma <biruklemmadebela@gmail.com>
2024-07-31 22:16:30 +0300

    client removed

--- src/App.jsx
+++ src/App.jsx
@@ -94,12 +94,11 @@ function App() {
             
 
             
-            <div className='text-slate-100 dark:text-slate-900 '>
+            {/* <div className='text-slate-100 dark:text-slate-900 '>
               <div className='max-w-screen-xl my-0 mx-auto w-11/12 '>
-                {/* <Clients /> */}
                 <CenterMode />
               </div>
-            </div>
+            </div> */}
 
             <div className='text-slate-100 dark:text-slate-900 '>
               <div className='max-w-screen-xl my-0 mx-auto w-11/12 '>
