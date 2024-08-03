9b3371b79cd9719a06ca7a6af839a15f03b84383
Biruk Lemma <biruklemmadebela@gmail.com>
2024-08-03 08:48:55 +0300

    about me updated

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -40,8 +40,7 @@ function AboutPage() {
         <div className='sm:flex gap-x-4 bg-inherit bg-white p-5 rounded-2xl sm:w-11/12 shadow-xl'>
           <div className='sm:w-1/2 pt-8 pl-3'>
             <p className=' pl-1 font-RobotoCondensed dark:font-semibold '>
-              My passion for technology and innovation began in my senior year of high school when I tried to build library management system for my highschool - turns out there's so much to you could do further more. Fast forward to today, and I've had the privilege of building software for businesses, and startups.
-              These days, my main focus is on developing web and mobile technology-based products and impactful projects.
+            My passion for technology and innovation began when I joined college, where I quickly discovered my knack for programming and software development. These days, my main focus is on developing web-based products and impactful projects. I'm a fast learner, and whenever I'm not working on a project, I'm always learning something new.
             </p>
 
             <PrimaryButton text={'Download Resume'} class={'mt-6 text-white'} onclick={handleDownload} />
@@ -52,14 +51,14 @@ function AboutPage() {
             {progressVisible && (
               <>
                 <Progress
-                  name={'Development'}
+                  name={'API Develpment'}
                   percent={90}
                   barColor={'bg-yellow-400'}
                   bgColor={'bg-slate-200'}
                 />
 
                 <Progress
-                  name={'UI/UX'}
+                  name={'Frontend Development'}
                   percent={79}
                   class={'mt-3'}
                   barColor={'bg-red-500'}
@@ -67,7 +66,7 @@ function AboutPage() {
                 />
 
                 <Progress
-                  name={'Mobile App'}
+                  name={'UI/UX Principles'}
                   percent={75}
                   class={'mt-3'}
                   barColor={'bg-blue-500'}
